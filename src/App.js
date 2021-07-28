e8d038182fdd407df68d70a84bd13499cb5b2c8f
Dibyanshu Jaiswal <uname2334@gmail.com>
2021-07-28 18:25:15 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+// components
+import Messenger from "./components/Messenger";
+
+const App = () => {
+    return (
+      <Messenger />
+    );
+}
+
+export default App;
---
+++ src/components/Messenger.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Messenger = () => {
+    return(
+        <p>Hello from Messenger</p>
+    )
+}
+
+export default Messenger;
